85a584b18c12b0d7b3a829769ba8ea84b47999ee
adhithan-m <116782079+adhithan-m@users.noreply.github.com>
2022-12-26 19:54:30 +0530

    duplicate number.

---
+++ duplicate-number/js/dup-num.js
@@ -0,0 +1,18 @@
+// var a = parseInt(prompt("Enter the length"));
+// var s =[];
+// var b =[];
+// for(i=0;i<a;i++)
+// {
+	// var temp =parseInt(prompt("Enter the value"));
+	// s.push(temp)
+// }
+// console.log(s);
+// var v = s.length     //s=[1,2,2,3,4,3,4,5]
+// for(i=0;i<v;i++)
+// {
+	// if(!b.includes(s[i]))
+	// {
+	// b.push(s[i])	
+	// }
+// }
+// console.log(b);
